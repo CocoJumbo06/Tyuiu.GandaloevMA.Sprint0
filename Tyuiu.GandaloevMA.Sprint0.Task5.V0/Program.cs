f0d488c3774e70be2c3d3dfeaacb2faa8926df23
Tyuiu.GandaloevMG@outlook.com <mganomattermga6027@gmail.com>
2023-09-26 17:09:36 +0300

    Task5

--- Tyuiu.GandaloevMA.Sprint0.Task5.V0/Program.cs
+++ Tyuiu.GandaloevMA.Sprint0.Task5.V0/Program.cs
@@ -3,21 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tyuiu.GandaloevMA.Sprint0.Task4.V0.Lib;
-namespace Tyuiu.GandaloevMA.Sprint0.Task4.V0
+using Tyuiu.GandaloevMA.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.GandaloevMA.Sprint0.Task5.V0
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             //Метод сложения Addition
-            Console.WriteLine(DataService.Addition(1, 5));
+            Console.WriteLine("A+B="+DataService.Addition(1, 5));
             //Метод вычетания Subtraction
-            Console.WriteLine(DataService.Subtraction(10, 5));
+            Console.WriteLine("A-B=" + DataService.Subtraction(10, 5));
             //Метод умножения Multiplication
-            Console.WriteLine(DataService.Multiplication(3, 5));
+            Console.WriteLine("A*B=" + DataService.Multiplication(3, 5));
             //Метод деления Division
-            Console.WriteLine(DataService.Division(10, 5));
+            Console.WriteLine("A/B=" + DataService.Division(10, 5));
             // Задержка экрана 
             Console.ReadKey();
         }
